15cd5ce0a2ab36d94d1b6bd7342234c934e72521
LimeyGreeny <76579448+LimeyGreeny@users.noreply.github.com>
2021-10-24 05:21:43 +0800

    Texture Pack Done

---
+++ Assets/Light Flicker/FlickeringControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringControl : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.01f, 1.0f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.01f, 1.0f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
