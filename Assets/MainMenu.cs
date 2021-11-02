e78707924e9f8fd879ae50082e8ec5b99acaca37
LimeyGreeny <76579448+LimeyGreeny@users.noreply.github.com>
2021-11-02 12:20:54 +0800

    Update Cinematic
    -Intro

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,10 +7,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     Animator animator;
+    VideoPlayer vid;
 
     public void Start()
     {
         animator = GetComponent<Animator>();
+        vid = GetComponentInChildren<VideoPlayer>();
+        vid.gameObject.SetActive(false) ;
     }
 
     public void PlayGame()
@@ -20,8 +23,19 @@ public class MainMenu : MonoBehaviour
 
     IEnumerator transition()
     {
+        GameObject fade = GameObject.FindWithTag("Fade"),
+            play = GameObject.FindWithTag("btnPlay"),
+            exit = GameObject.FindWithTag("btnExit");
+        fade.SetActive(false);
+        play.SetActive(false);
+        exit.SetActive(false);
         animator.SetTrigger("triggerA");
         yield return new WaitForSeconds(2f);
+        vid.gameObject.SetActive(true);
+        Debug.Log("Video is Active: " + vid.gameObject.activeInHierarchy);
+        yield return new WaitForSeconds((float)vid.length + 1f);
+        fade.SetActive(true);
+        vid.gameObject.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
